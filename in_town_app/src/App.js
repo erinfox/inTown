ab4143165e83fa6716826f67b83646f80d239dff
erin fox <erinkayfox@gmail.com>
2017-05-16 16:53:46 -0400

    one marker working

--- in_town_app/src/App.js
+++ in_town_app/src/App.js
@@ -4,17 +4,32 @@ import './App.css';
 import GoogleMaps from './Components/Container';
 import CheckBox from './Components/CheckBox';
 import './animate.css';
+import Markers from './Components/Markers';
 
 
 class App extends Component {
-
+  constructor() {
+    super();
+    this.state = {
+      places: [ {place:"stefan's house", lat: 40.7611, lng: -73.8650}]
+    };
+    this.addToPlaces = this.addToPlaces.bind(this);
+  }
+  addToPlaces(obj) {
+    let currentPlaces = this.state.places;
+    currentPlaces.push(obj);
+    this.setState({
+      places: currentPlaces
+    })
+  }
   render() {
     return (
       <div className="App">
         <h1 className="inTown">inTown<img src={require('../media/taxi.png')} className="lightSpeedIn animated taxiSize" /></h1>
-          <div className="boxAndGoogleMap">
-          <div className="CheckBox"><CheckBox /></div>
-          <div className="GoogleMaps"><GoogleMaps /></div>
+        <div className="boxAndGoogleMap">
+          <div className="CheckBox"><CheckBox addToPlaces={this.addToPlace} /></div>
+          <div className="GoogleMaps"><GoogleMaps places={this.state.places}/></div>
+          {/*<renderMarkers />*/}
         </div>
       </div>
 
--- in_town_app/src/Components/Container.js
+++ in_town_app/src/Components/Container.js
@@ -1,17 +1,40 @@
 import React, { Component } from 'react';
-import Map, {GoogleApiWrapper} from 'google-maps-react'
-// import Marker, { Component } from 'google-maps-react'
+import Map, {GoogleApiWrapper, Marker} from 'google-maps-react';
 
 class Container extends Component {
+  constructor() {
+    super();
+
+    this.renderMarkers = this.renderMarkers.bind(this);
+  }
   onReady(mapProps, map){
     //when map is ready and loaded
   }
+  renderMarkers(){
+    return this.props.places.map( (place, index) => {
+      return (
+          <Marker
+            key={index}
+            name={place.place}
+            position={{ lat: place.lat, lng:place.lng }} />
+        )
+    })
+  };
   render() {
     const style = {
       width: '55vw',
       height: '55vh'
     }
-    return  <Map style={style}  google={this.props.google} zoom={12} />;
+    return (
+    <Map style={style}
+      zoom={12}
+      onReady={this.onReady.bind(this)}
+      google={this.props.google}
+      mapCenter={{lat: 40.7611, lng: -73.8650}}
+      >
+      { this.renderMarkers() }
+    </Map>
+    )
   }
 }
 
